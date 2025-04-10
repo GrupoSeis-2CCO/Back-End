3793210ec8d77768e480277b3c834f3c443bc927
ArthurRamoss <arthur.santos@sptech.school>
2025-04-10 20:34:18 -0300

    [INCOMPLETO] Inicio Crud - Usuario

--- crud-gratitude-servicos/crud-gratitude-servicos/src/main/java/servicos/gratitude/crud_gratitude_servicos/controller/UsuarioController.java
+++ crud-gratitude-servicos/crud-gratitude-servicos/src/main/java/servicos/gratitude/crud_gratitude_servicos/controller/UsuarioController.java
@@ -2,19 +2,22 @@ package servicos.gratitude.crud_gratitude_servicos.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import servicos.gratitude.crud_gratitude_servicos.entity.Usuario;
 import servicos.gratitude.crud_gratitude_servicos.entity.dto.UsuarioRequestDto;
 import servicos.gratitude.crud_gratitude_servicos.entity.dto.UsuarioResponseDto;
+import servicos.gratitude.crud_gratitude_servicos.entity.dto.mapper.UsuarioMapper;
 import servicos.gratitude.crud_gratitude_servicos.service.UsuarioService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     private final UsuarioService service;
+
 
     public UsuarioController(UsuarioService service) {
         this.service = service;
@@ -24,6 +27,46 @@ public class UsuarioController {
     public ResponseEntity<UsuarioResponseDto> cadastrar(
             @RequestBody @Valid UsuarioRequestDto request
             ){
-        return null;
+        if(service.existsByEmailAndCpf(request)){
+            return ResponseEntity.status(409).build();
+        }
+
+        UsuarioMapper mapper = new UsuarioMapper();
+
+        Usuario usuarioNovo = mapper.toEntity(request);
+        service.cadastrarUsuario(usuarioNovo);
+        UsuarioResponseDto response = mapper.toEntity(usuarioNovo);
+        return ResponseEntity.status(201).body(response);
     }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDto>> listar(){
+
+        List<Usuario> usuarios = service.listarUsuarios();
+
+        if(usuarios.isEmpty()){
+            return ResponseEntity.status(204).build();
+        }
+
+        List<UsuarioResponseDto> responses = new ArrayList<>();
+        UsuarioMapper mapper = new UsuarioMapper();
+
+        for(Usuario usuario : usuarios){
+            responses.add(mapper.toEntity(usuario));
+        }
+        return ResponseEntity.status(200).body(responses);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity atualizarSenha(
+            @RequestParam String senha,
+            @PathVariable Integer id
+    ){
+        if(service.existsById(id)){
+            return ResponseEntity.status(404).build();
+        }
+        service.atualizarSenha(senha, id);
+        return ResponseEntity.status(200).build();
+    }
+
 }
--- crud-gratitude-servicos/crud-gratitude-servicos/src/main/java/servicos/gratitude/crud_gratitude_servicos/service/UsuarioService.java
+++ crud-gratitude-servicos/crud-gratitude-servicos/src/main/java/servicos/gratitude/crud_gratitude_servicos/service/UsuarioService.java
@@ -2,7 +2,11 @@ package servicos.gratitude.crud_gratitude_servicos.service;
 
 import org.springframework.stereotype.Service;
 import servicos.gratitude.crud_gratitude_servicos.entity.Usuario;
+import servicos.gratitude.crud_gratitude_servicos.entity.dto.UsuarioRequestDto;
+import servicos.gratitude.crud_gratitude_servicos.entity.dto.UsuarioResponseDto;
 import servicos.gratitude.crud_gratitude_servicos.repository.UsuarioRepository;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -11,6 +15,28 @@ public class UsuarioService {
 
     public UsuarioService(UsuarioRepository repository) {
         this.repository = repository;
+    }
+
+    public Boolean existsByEmailAndCpf(UsuarioRequestDto request) {
+        return repository.existsByEmailAndCpf(request.getEmail(), request.getCpf());
+    }
+
+    public List<Usuario> listarUsuarios() {
+        return repository.findAll();
+    }
+
+    public void cadastrarUsuario(Usuario usuario) {
+        repository.save(usuario);
+    }
+
+    public Boolean existsById(Integer id) {
+        return repository.existsById(id);
+    }
+
+    public void atualizarSenha(String senha, Integer idUsuario) {
+        Usuario usuarioParaAtualizar = repository.findById(idUsuario).get();
+        usuarioParaAtualizar.setSenha(senha);
+        repository.save(usuarioParaAtualizar);
     }
 
 }
