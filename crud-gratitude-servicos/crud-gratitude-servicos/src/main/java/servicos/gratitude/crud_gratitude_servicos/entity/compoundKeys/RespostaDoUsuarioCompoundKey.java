c184774689c66653db1cf0b2994ed7c4567ede3e
rogeriomatzak <rogerio.santiago@sptech.school>
2025-06-10 20:34:37 -0300

    Add Bildager Funcionando

--- crud-gratitude-servicos/crud-gratitude-servicos/src/main/java/servicos/gratitude/crud_gratitude_servicos/entity/compoundKeys/RespostaDoUsuarioCompoundKey.java
+++ crud-gratitude-servicos/crud-gratitude-servicos/src/main/java/servicos/gratitude/crud_gratitude_servicos/entity/compoundKeys/RespostaDoUsuarioCompoundKey.java
@@ -1,12 +1,14 @@
 package servicos.gratitude.crud_gratitude_servicos.entity.compoundKeys;
 
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
 @Getter
 @Setter
+@EqualsAndHashCode
 public class RespostaDoUsuarioCompoundKey {
     private TentativaCompoundKey idTentativaComposto;
     private AlternativaCompoundKey idAlternativaComposto;
